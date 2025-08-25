92a30fb4d4de803f5c37659f2fcf12780ea63876
André Melchior <andre.melchior@outlook.com>
2025-08-25 03:21:13 -0300

    feat(ui): add greeting and login form sections in login screen - add rem units for consistent sizing - implement nesting and variables for color management - add form section with email and password inputs - add SCSS mixins for code reuse (dualFont() for mixed fonts, title() for h1 styling)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,56 @@
-import './App.scss'
+import styles from "./App.module.scss";
 
 export function App() {
-  return (
-    <div>
-        <main>
-            <div className=''></div>
-            <div className=''>
-                <form action="">
-                    <input type="email" name="" id="" />
-                    <input type="password" name="" id="" />
-                    <h3>Esqueci minha senha.</h3>
-                </form>
+    return (
+        <main className={styles.main}>
+            <div className={styles.contentWelcome}>
+                <div className={styles.title}>
+                    <h1 className={styles.kaloomTitle}>kaloom</h1>
+                </div>
+                <h2 className={styles.subtitle}>
+                    Olá
+                    <strong>
+                        <span>,</span> <br />
+                        bem-vindo<span>!</span>
+                    </strong>
+                </h2>
+                <p className={styles.paragrafo}>
+                    Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem
+                    ipsum dolor sit amet lorem ipsum dolor sit amet.
+                </p>
+            </div>
+
+            <div className={styles.containerLogin}>
+                <div className={styles.contentLogin}>
+                    <form action="" className={styles.form}>
+                        <h3 className={styles.h3}>login</h3>
+                        <input
+                            className={styles.input}
+                            type="email"
+                            name=""
+                            id=""
+                            placeholder="email"
+                        />
+                        <input
+                            className={styles.input}
+                            type="password"
+                            name=""
+                            id=""
+                            placeholder="senha"
+                        />
+                        <a className={styles.a} href="#">
+                            Esqueci minha senha.
+                        </a>
+                    </form>
+                </div>
+                <hr className={styles.divider} />
+                <p className={styles.linkCadastro}>
+                    Não tem uma conta?
+                    <b>
+                        <a href="#"> Cadastre-se</a>
+                    </b>
+                </p>
             </div>
         </main>
-    </div>
-  )
+    );
 }
