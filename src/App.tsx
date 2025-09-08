22d2fc2a92b1889570b54059db57d4475a2e37ec
André Melchior <andre.melchior@outlook.com>
2025-09-07 23:39:21 -0300

    feat: add "/" and "login" route paths with React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
+
 
 export function App() { 
     return (
-        <Login />
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <button
+                            style={{
+                                width: 80,
+                                height: 35,
+                                backgroundColor: "#996dff",
+                                margin: 20,
+                                border: "none",
+                                display: "flex",
+                            }}
+                        >
+                            <a
+                                href="/login"
+                                style={{
+                                    textDecoration: "none",
+                                    color: "white",
+                                    width: "100%",
+                                    height: "100%",
+                                    lineHeight: 2.5,
+                                    fontWeight: "bold"
+                                }}
+                            >
+                                Login
+                            </a>
+                        </button>
+                    }
+                />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
