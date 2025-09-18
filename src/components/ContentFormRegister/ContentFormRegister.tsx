f346345836e7b81552127afde21e23d0b63b3dc6
Alisson Ruan <alisson.gomes12@etec.sp.gov.br>
2025-09-17 22:22:36 -0300

    feat: add buttons in register form

--- src/components/ContentFormRegister/ContentFormRegister.tsx
+++ src/components/ContentFormRegister/ContentFormRegister.tsx
@@ -1,11 +1,12 @@
-import styles from "./ContentFormRegister.module.scss";
+import styles from "../ContentFormLogin/ContentFormLogin.module.scss";
+import stylesB from "./ContentFormRegister.module.scss";
 import RegisterInput from "../SignInput/SignInput";
 import SignButton from "../SignButton/SignButton";
 
 export default function ContentFormRegister() {
     return (
         <div className={styles.contentLogin}>
-            <form action="" className={styles.form}>
+            <form className={styles.form}>
                 <h3 className={styles.h3}>cadastro</h3>
                 <div style={{ display: "flex", flexDirection: "row", gap: "2rem" }}>
                     <RegisterInput type={"text"} placeholder={"nome"} />
@@ -14,6 +15,12 @@ export default function ContentFormRegister() {
                 <RegisterInput type={"text"} placeholder={"@username"} />
                 <RegisterInput type={"email"} placeholder={"email"}/>
                 <RegisterInput type={"password"} placeholder={"senha"} />
+
+                <p style={{alignSelf: "flex-start", fontFamily:"Sen", opacity:"50%", color:"white"}}>Selecione qual instituição estuda/estudou</p>
+                <div style={{display: "flex", gap: 10, alignSelf: "flex-start" }}>
+                    <SignButton textButton="Etec" styleButton={stylesB.signInButton} styleText={stylesB.textEntrar} />
+                    <SignButton textButton="Fatec" styleButton={stylesB.signInButton} styleText={stylesB.textEntrar} />
+                </div>
                 <SignButton textButton="Cadastrar" styleButton={styles.signInButton} styleText={styles.textEntrar} />
             </form>
         </div>
