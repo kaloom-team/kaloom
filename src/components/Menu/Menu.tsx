660d707ecbc0e2650895a47a060a6d21bb2d1116
Alisson Ruan <alisson.gomes12@etec.sp.gov.br>
2025-10-22 22:25:22 -0300

    feat: add component "Menu" with links list and button component from shadcn/ui

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,19 @@
+import styles from '@/components/ContentFormLogin/ContentFormLogin.module.scss';
+import { Button } from '../ui/button';
+
+
+export default function Menu() {
+    return (
+        <div className='flex gap-[2.188rem]'>
+            <a href="" className='text-white'>Home</a>
+            <a href="" className='text-white'>Suporte</a>
+            <a href="" className='text-white'>Contato</a>
+
+            <div id='buttons' className='flex gap-[0.625rem]'>
+
+                <Button variant="outline" className='rounded-[3.125rem] w-[4.688rem] h-[2.438rem] bg-[#6C27B3] text-white border-none'>Login</Button>    
+                <Button variant="outline" className='rounded-[3.125rem] w-[4.688rem] h-[2.438rem] bg-[#6C27B3] text-white border-none'>Cadastro</Button>    
+            </div>
+        </div>
+    )
+}
