87b0578823db6f0d9b392a9e42dcb53132108ea5
Alisson Ruan <Alisson Ruan>
2025-11-01 00:42:01 -0300

    feat: change accordion text items

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/ui/accordion"
 
 export default function Main() {
+
     return (
         <main className="relative h-screen w-screen flex flex-col bg-[url('/bg-main.png')] bg-cover bg-no-repeat bg-top">
             <Header />
@@ -25,44 +26,19 @@ export default function Main() {
                 <AccordionItem value="item-1">
                     <AccordionTrigger className="text-white">Sobre o kaloom</AccordionTrigger>
                     <AccordionContent className="flex flex-col gap-4 text-balance text-white">
-                    <p>
-                        Our flagship product combines cutting-edge technology with sleek
-                        design. Built with premium materials, it offers unparalleled
-                        performance and reliability.
-                    </p>
-                    <p>
-                        Key features include advanced processing capabilities, and an
-                        intuitive user interface designed for both beginners and experts.
-                    </p>
+                    <p>O Kaloom é uma plataforma que introduz a interação entre Alunos e Ex-Alunos de instituicões de Ensino Etec e Fatec.</p>
                     </AccordionContent>
                 </AccordionItem>
                 <AccordionItem value="item-2">
                     <AccordionTrigger className="text-white">Termos de Serviço</AccordionTrigger>
                     <AccordionContent className="flex flex-col gap-4 text-balance text-white">
-                    <p>
-                        We offer worldwide shipping through trusted courier partners.
-                        Standard delivery takes 3-5 business days, while express shipping
-                        ensures delivery within 1-2 business days.
-                    </p>
-                    <p>
-                        All orders are carefully packaged and fully insured. Track your
-                        shipment in real-time through our dedicated tracking portal.
-                    </p>
+                    <p>Estes Termos de Serviço refletem a maneira como os negócios do Kaloom funcionam, ajudando a definir a relação do software com você. <a className="underline" href="#">Clique Aqui</a> para saber mais sobre os Termos de Serviço</p>
                     </AccordionContent>
                 </AccordionItem>
                 <AccordionItem value="item-3">
-                    <AccordionTrigger className="text-white">Politicas</AccordionTrigger>
+                    <AccordionTrigger className="text-white">Politicas de Privacidade</AccordionTrigger>
                     <AccordionContent className="flex flex-col gap-4 text-balance text-white">
-                    <p>
-                        We stand behind our products with a comprehensive 30-day return
-                        policy. If you&apos;re not completely satisfied, simply return the
-                        item in its original condition.
-                    </p>
-                    <p>
-                        Our hassle-free return process includes free return shipping and
-                        full refunds processed within 48 hours of receiving the returned
-                        item.
-                    </p>
+                    <p>Entendemos que é uma grande responsabilidade e trabalhamos para proteger seus dados. <a href="#" className="underline">Saiba mais</a> sobre as Politicas de Pivacidade.</p>
                     </AccordionContent>
                 </AccordionItem>
                 </Accordion>
