51ced19ad89aa8e3b86400b9c7f92a6f80637061
André Melchior <andre.melchior@outlook.com>
2025-11-02 18:27:05 -0300

    feat(login): implement React form event and API integration
    - add form event handling for login inputs
    - add Axios to consume login API endpoint
    - include SweetAlert2 for success, verification and error messages

--- src/components/ContentFormLogin/ContentFormLogin.tsx
+++ src/components/ContentFormLogin/ContentFormLogin.tsx
@@ -1,30 +1,127 @@
 import styles from "./ContentFormLogin.module.scss";
 import LoginInput from "../SignInput/SignInput";
 import SignButton from "../SignButton/SignButton";
+import { useState } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 export default function ContentFormLogin() {
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const port = "7020";
+    const url = `https://localhost:${port}/api/Usuario`;
+
+    const loginSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const userLogin = {
+            email,
+            senha,
+        };
+
+        function errorAlert(html: string) {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                html: html,
+                footer: '<a href="#">Por que estou tendo esse problema?</a>',
+            });
+        }
+
+        await Swal.fire({
+            title: "Verificando...",
+            icon: "info",
+            // timer: 2000,
+            didOpen: async () => {
+                Swal.showLoading();
+                setTimeout(async () => {
+                    try {
+                        const response = await axios.post(`${url}/Login`, userLogin);
+                        console.log("Login realizado com sucesso: ", response.data);
+                        window.location.assign("http://localhost:5173/")
+                    } catch (err: any) {
+                        console.error(err);
+                        if (email == "" || senha == "") { 
+                            await errorAlert(
+                                "Erro ao realizar o login :( <br /> Preencha os campos vazios."
+                            );
+                        } else if (err.response?.status == 401) {
+                            await errorAlert(
+                                "Erro ao realizar o login :( <br /> Email ou senha incorretos."
+                            );
+                        } else {
+                            await errorAlert(
+                                "Erro ao realizar o login :( <br /> Tente novamente mais tarde."
+                            );
+                        }
+                    }
+                }, 2000)
+            },
+        });
+    };
+
     return (
         <div className={styles.contentLogin}>
-            <form action="" className={styles.form}>
+            <form action="" className={styles.form} onSubmit={loginSubmit}>
                 <h3 className={styles.h3}>login</h3>
-                <LoginInput type={"email"} placeholder={"email"}/>
-                <LoginInput type={"password"} placeholder={"senha"} />
-                <SignButton textButton="Entrar" styleButton={styles.signInButton} styleText={styles.textEntrar}/>
-                <div style={{ display: "flex", width: "100%", alignItems: "center", justifyContent: "center" }}>
+                <LoginInput
+                    type={"email"}
+                    placeholder={"email"}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <LoginInput
+                    type={"password"}
+                    placeholder={"senha"}
+                    value={senha}
+                    onChange={(e) => setSenha(e.target.value)}
+                />
+                <SignButton
+                    textButton="Entrar"
+                    type="submit"
+                    styleButton={styles.signInButton}
+                    styleText={styles.textEntrar}
+                />
+                <div
+                    style={{
+                        display: "flex",
+                        width: "100%",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}
+                >
                     <hr className={styles.dividerAuth} />
-                    <div style={{marginLeft: 13, marginRight: 13 }}>
-                        <p style={{fontFamily: "Sen", fontSize: 20, color: "white"}}>ou entrar com</p>
+                    <div style={{ marginLeft: 13, marginRight: 13 }}>
+                        <p
+                            style={{
+                                fontFamily: "Sen",
+                                fontSize: 20,
+                                color: "white",
+                            }}
+                        >
+                            ou entrar com
+                        </p>
                     </div>
                     <hr className={styles.dividerAuth} />
                 </div>
                 <div className="flex flex-row gap-[2rem]">
-                    <SignButton img="/google-auth.svg" textButton="Google" styleButton={styles.authButton} styleText={styles.textRede}/>
-                    <SignButton img="/github-auth.svg" textButton="GitHub" styleButton={styles.authButton} styleText={styles.textRede}/>
+                    <SignButton
+                        img="/google-auth.svg"
+                        textButton="Google"
+                        styleButton={styles.authButton}
+                        styleText={styles.textRede}
+                    />
+                    <SignButton
+                        img="/github-auth.svg"
+                        textButton="GitHub"
+                        styleButton={styles.authButton}
+                        styleText={styles.textRede}
+                    />
                 </div>
                 <a className={styles.forgot} href="#">
                     Esqueci minha senha.
                 </a>
             </form>
         </div>
-    )
+    );
 }
